4d0c4940596405b45cc95ce2155afd26325ef26e
Atos Singh <sheemaratos@gmail.com>
2025-06-10 01:21:34 +0200

    seconda parte

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_image.h>
 #include<SDL3/SDL_audio.h>
-
+#include<SDL3/SDL_ttf.h>
  /* We will use this renderer to draw into this window every frame. */
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
@@ -30,23 +30,37 @@ bool playing = false;  /* flag to check if audio is playing */
 char* UpH_path = NULL;  
 SDL_Surface* UpH_surface = NULL;  
 SDL_Texture* UpH_texture = NULL;  
+SDL_FRect UpH_rect = {86, 20, 224, 176};
 
 char* DownH_path = NULL;  
 SDL_Surface* DownH_surface = NULL;  
-SDL_Texture* DownH_texture = NULL;  
+SDL_Texture* DownH_texture = NULL;
+SDL_FRect DownH_rect = {138, 394, 314, 202};  
 
 char* LeftH_path = NULL;  
 SDL_Surface* LeftH_surface = NULL;  
 SDL_Texture* LeftH_texture = NULL;  
+SDL_FRect LeftH_rect = {0, 206, 226, 170};
 
 char* RightH_path = NULL;  
 SDL_Surface* RightH_surface = NULL;  
 SDL_Texture* RightH_texture = NULL;  
+SDL_FRect RightH_rect = {324, 182, 272, 202};
 
 char* Cover_path = NULL;  
 SDL_Surface* Cover_surface = NULL;  
 SDL_Texture* Cover_texture = NULL;  
+SDL_FRect Cover_rect = {0, 0, 300, 300};
 
+char* font_path = NULL;  /* path to the font file */
+TTF_Font *font = NULL;  /* font for rendering text */
+SDL_Surface *text_surface = NULL;  /* surface for rendering text */
+SDL_Texture *text_texture = NULL;  /* texture for rendering text */
+
+
+
+void TextShow();
+double AudioPos();
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
@@ -62,6 +76,28 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    if(!TTF_Init()) {
+        SDL_Log("Couldn't initialize TTF: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_asprintf(&font_path, "%s\\font\\BodoniModa.ttf", SDL_GetBasePath());
+    font = TTF_OpenFont(font_path, 24);
+    if (!font) {
+        SDL_Log("Couldn't open font: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    text_surface = TTF_RenderText_Blended(font, "Convenience", 0, {0,0,0, 255});
+    if (!text_surface) {
+        SDL_Log("Couldn't render text: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+    if (!text_texture) {
+        SDL_Log("Couldn't create texture from surface: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
     SDL_asprintf(&wav_path, "%s\\audio\\convenience [gqt0BC2bkJY].wav", SDL_GetBasePath());
     if(!SDL_LoadWAV(wav_path, &audio_spec, &audio_buf, &audio_len)) {
         SDL_Log("Couldn't load WAV file: %s", SDL_GetError());
@@ -74,13 +110,13 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     Cover_surface = IMG_Load(Cover_path);
     SDL_SetWindowIcon(window, Cover_surface);
 
-    SDL_asprintf(&UpH_path, "%s\\images\\UpH.jpg", SDL_GetBasePath());
+    SDL_asprintf(&UpH_path, "%s\\images\\UpH.png", SDL_GetBasePath());
     UpH_surface = IMG_Load(UpH_path);
-    SDL_asprintf(&DownH_path, "%s\\images\\DownH.jpg", SDL_GetBasePath());
+    SDL_asprintf(&DownH_path, "%s\\images\\DownH.png", SDL_GetBasePath());
     DownH_surface = IMG_Load(DownH_path);
-    SDL_asprintf(&LeftH_path, "%s\\images\\LeftH.jpg", SDL_GetBasePath());
+    SDL_asprintf(&LeftH_path, "%s\\images\\LeftH.png", SDL_GetBasePath());
     LeftH_surface = IMG_Load(LeftH_path);
-    SDL_asprintf(&RightH_path, "%s\\images\\RightH.jpg", SDL_GetBasePath());
+    SDL_asprintf(&RightH_path, "%s\\images\\RightH.png", SDL_GetBasePath());
     RightH_surface = IMG_Load(RightH_path);
     if (!Cover_surface ||!UpH_surface || !DownH_surface || !LeftH_surface || !RightH_surface) {
         SDL_Log("Couldn't load image: %s", SDL_GetError());
@@ -92,6 +128,11 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
     DownH_texture = SDL_CreateTextureFromSurface(renderer, DownH_surface);
     LeftH_texture = SDL_CreateTextureFromSurface(renderer, LeftH_surface);
     RightH_texture = SDL_CreateTextureFromSurface(renderer, RightH_surface);
+    SDL_SetTextureScaleMode(Cover_texture, SDL_SCALEMODE_NEAREST);
+    SDL_SetTextureScaleMode(UpH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(DownH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(LeftH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(RightH_texture, SDL_SCALEMODE_NEAREST);
     if (!Cover_texture || !DownH_texture || !LeftH_texture || !RightH_texture) {
 
         SDL_Log("Couldn't create texture from surface: %s", SDL_GetError());
@@ -142,7 +183,12 @@ SDL_AppResult SDL_AppIterate(void* appstate)
 {
     SDL_SetRenderDrawColorFloat(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE_FLOAT);  /* new color, full alpha. */
     /* clear the window to the draw color. */
+    double now = double(SDL_GetTicks()) / 1000.0;  /* get the current time in seconds. */
+    double sin1 = SDL_sin(now);  /* just to make sure we don't get a warning about unused variables. */
+
     SDL_RenderClear(renderer);
+
+
     if(started) {
         if(SDL_GetAudioStreamQueued(audio_stream) < int(audio_len) && !playing) {
             SDL_PutAudioStreamData(audio_stream, audio_buf, audio_len);
@@ -151,13 +197,28 @@ SDL_AppResult SDL_AppIterate(void* appstate)
             SDL_Log("Audio length: %d bytes", int(audio_len));
             playing = true;  /* set the flag to true */
         }
+        UpH_rect.x = 86 + 10 * sin1;  /* move the texture around a bit. */
+        LeftH_rect.y = 206 + 10 * sin1;  /* move the texture around a bit. */
+        DownH_rect.x = 138 + 10 * -sin1;  /* move the texture around a bit. */
+        RightH_rect.y = 182 + 10 * -sin1;  /* move the texture around a bit. */
+        SDL_RenderTexture(renderer, UpH_texture, NULL, &UpH_rect);  /* render the texture to the window. */
+        SDL_RenderTexture(renderer, DownH_texture, NULL, &DownH_rect);  /* render the texture to the window. */
+        SDL_RenderTexture(renderer, LeftH_texture, NULL, &LeftH_rect);  /* render the texture to the window. */
+        SDL_RenderTexture(renderer, RightH_texture, NULL, &RightH_rect);  /* render the texture to the window. */
+        
+        SDL_SetTextureScaleMode(UpH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(DownH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(LeftH_texture, SDL_SCALEMODE_NEAREST);
+        SDL_SetTextureScaleMode(RightH_texture, SDL_SCALEMODE_NEAREST);
 
-        SDL_RenderTexture(renderer, Cover_texture, NULL, NULL);  /* render the texture to the window. */
+
+        TextShow();
 
         if( SDL_GetAudioStreamQueued(audio_stream) == 0) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
     }
+    SDL_RenderTexture(renderer, text_texture, NULL, NULL);  /* render the texture to the window. */
     /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(renderer);
 
@@ -172,3 +233,22 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result)
 
 
 
+void TextShow(){
+    double pos = AudioPos();  /* get the audio position */
+    char* Testo = "";
+    SDL_Log("Audio position: %f seconds", pos);
+
+    text_surface = TTF_RenderText_Blended(font, Testo, 0, {0,0,0, 255});
+    text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+
+
+}
+
+double AudioPos(){
+    double queued = SDL_GetAudioStreamQueued(audio_stream);
+    double bytes_per_second = audio_spec.freq * audio_spec.channels * SDL_AUDIO_BYTESIZE(audio_spec.format);  /* calculate the bytes per second */
+    double seconds_remaining = queued / bytes_per_second;  /* calculate the seconds remaining */
+    double full_seconds = audio_len / bytes_per_second;  /* calculate the full seconds of audio */
+    //SDL_Log("Audio data read from stream: %f bytes, len: %f, now: %f", seconds_remaining, full_seconds, full_seconds - seconds_remaining);
+    return full_seconds-seconds_remaining;  /* return the seconds remaining */
+}
